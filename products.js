3b280a101406796672d12e12a65863adb7df71a5
guijjane <guijjaneabdelghaffar85@gmail.com>
2023-04-06 16:23:57 +0000

    backendprjetjean

--- products.js
+++ products.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 5000
 const { request } = require('http')
 const { response } = require('express')
 const { Console } = require('console')
 //utilis le app .use pour lir le json
+const cors=require('cors')
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+
 app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 const requettes = require('./requettes')
 //let client = requettes.getConnect()
 const client = require('./requettes')
+const secret = 'ma-cle-secrete';
+const User = require("./requettes");
+app.use(cors({
+  credentials:true,
+  origin:'http://localhost:3000'
+}));
+
 
 app.get('/products', (req, res) => {
     let client =requettes.client
@@ -20,7 +34,7 @@ app.get('/products', (req, res) => {
      
           
     }
-    requettes.getAllProduct(client,callback)
+    requettes.GetAllProduct(client,callback)
   
 });
 
@@ -33,7 +47,7 @@ app.get('/all', (req, res) => {
 app.get('/products/:id', (req, res) => {
   let id = req.params.id;
   let client=requettes.client 
-   requettes.getProductId(client,id,(error,data)=>{
+   requettes.GetProductId(client,id,(error,data)=>{
     if(error){
         console.log(error)
         return
@@ -46,11 +60,11 @@ app.get('/products/:id', (req, res) => {
 });
 
 // Ajout d'un produit
-app.post('/Ajproducts', (req, res) => {
+app.post('/ajproducts', (req, res) => {
 let client=requettes.client 
   let {id, name, description, price, stock } = req.body;
   let newProduct = { id, name, description, price, stock };
-  requettes.postProduct(newProduct,client,(error,data)=>{
+  requettes.PostProduct(newProduct,client,(error,data)=>{
     if(error){
         console.log(error)
         return
@@ -62,12 +76,12 @@ let client=requettes.client
 });
 
 // Mise à jour d'un produit selon son ID
-app.put('/MJproducts/:id', (req, res) => {
+app.put('/mjproducts/:id', (req, res) => {
     let client=requettes.client;
     let id = req.params.id;
     let { name, description, price, stock } = req.body;
     let newProduct1 = { id, name, description, price, stock };  
-     requettes.putProduct(id,newProduct1,client, (error,data) => {
+     requettes.PutProduct(id,newProduct1,client, (error,data) => {
       if (error){
         console.log(error)
         res.status(500).send("Erreur lors de la mise à jour du produit");
@@ -79,10 +93,10 @@ app.put('/MJproducts/:id', (req, res) => {
   });
 
 // Suppression d'un produit selon son ID
-app.delete('/Supproducts/:id', (req, res) => {
+app.delete('/supproducts/:id', (req, res) => {
     let client=requettes.client 
     let id = req.params.id;
-    requettes.deleteProduct(id,client, (error, data) => {
+    requettes.DeleteProduct(id,client, (error, data) => {
       if (error) {
         console.log(error)
         res.status(500).send("Erreur lors de la suppression du produit");
@@ -93,9 +107,187 @@ app.delete('/Supproducts/:id', (req, res) => {
     
   });
 
+ 
+
+
+//-----------------------------------------------------
+//backend de register 
+//la 1er methode avec hach code 
+app.post('/register', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    console.log(email,password)
+    let client = requettes.client;
+    let sql = 'SELECT * FROM users WHERE email = $1';
+    client.query(sql, [email], (error,result) =>{
+      if (error){
+        console.error("Error checking if user exists: ", error);
+        return res.status(500).json({ message: 'Internal server error' });
+      }
+      if (result.rows.length > 0) {
+        return res.status(409).json({ message: "Email already registered" });
+      }
+      bcrypt.hash(password, 10, (error, hashedPassword) => {
+        if (error) {
+          console.error("Error hashing password: ", error);
+          return res.status(500).json({ message: "Internal server error" });
+        }
+        const newUser = [req.body.firstname, req.body.lastname, req.body.email, hashedPassword]
+        requettes.addUser(client, newUser, callback)
+        function callback (error,data){
+          if (error) {
+            console.error("Error saving user: ", error);
+            return res.status(500).json({ message: "Internal server error" });
+          }
+          res.json({message:"User Added successfully"})}
+        });
+      });
+    });
+  
+
+// la 2eme methode sans hache
+// function getRegisteredUsersFromDb() {
+//   return new Promise((resolve, reject) => {
+//     let client = requettes.client;
+//     requettes.GetAllEmailUsers(client, (err, data) => {
+//       if (err) {
+//         console.log('Erreur lors de la récupération des utilisateurs enregistrés : ', err);
+//         reject(err);
+//       } else {
+//         const users = data.rows.map(row => row.email);
+//         resolve(users);
+//       }
+//     });
+//   });
+// }
+// async function checkCredentials2(email) {
+//   const users = await getRegisteredUsersFromDb();
+//   const user = users.find(u => u === email);
+//   console.log(user);
+//   return user;
+
+// }
+// app.post('/register', async (req, res) => {
+//  let {firstname, lastname, email, password} = req.body;
+//   const userExists = await checkCredentials2(email);
+//   if (userExists) {
+//     res.status(401).json({ message: 'Email exists' });
+//     return;
+//   } else {
+//       let newUser = {firstname,lastname,email,password};
+//       let client = requettes.client;
+//       requettes.PostRegister(newUser, client, (error, data) => {
+//         if (error) {
+//           console.log(error);
+//           return;
+//         }
+//       res.status(201).json(newUser);
+//     });
+//     }
+// });
+
+//----------------------------------------------------------
+//backend de login 
+async function checkCredentials(email, password) {
+  let client =requettes.client
+  try {
+    const result = await client.query('SELECT * FROM users WHERE email = $1 AND password = $2', [email, password]);
+    return result.rows[0];
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+app.post('/login',async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = await checkCredentials(email, password);
+  console.log(user);
+  if (!user) {
+    res.status(403).send('Wrong credentials');
+  } else {
+    const authToken = Math.random().toString();
+    res.cookie('authToken', authToken);
+    res.json({ message: 'Authentication success' });
+  }
+});
+
+// Middleware : la fonction pour authentifier l'utilisateur à chacune de ses requetes
+const authenticate =(req, res, next) => {
+  const headerToken = req.headers.authorization;
+  if (!headerToken) {
+     return res.status(401).json({ message: 'Authorization header missing' });
+  }
+  const token = headerToken.split(' ')[1];
+  try {
+     const decodedToken = jwt.verify(token, secret);
+     req.user = decodedToken;
+     next();
+  } catch (error) {
+     return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+app.use(authenticate)
+
+app.get('/products', authenticate,(req, res) => {
+  res.send('Bienvenue sur mon application !');
+});
 
 
 app.listen(port, () => {
     console.log(`Serveur démarré sur le port ${port}`);
   });
- 
+
+
+
+
+//---------------------------------------------------------------
+
+//   let  registereUsers = [
+//     {firstname: "admin",
+//     lastname: "admin",
+//     email: "admin@gmail.com",
+//     password: "azerty"},
+//     {firstname: "jalal",
+//     lastname: "jalal",
+//     email: "jalal@gmail.com",
+//     password: "azert"},
+//     {firstname: "monir",
+//     lastname: "monir",
+//     email: "monir@gmail.com",
+//     password: "azer"}
+//   ];
+//   function getRegisteredUsers() {
+//     return registereFindUser;
+//   }
+
+
+// function checkCredentials(email, password) {
+//   const users = getRegisteredUsers();
+//   const user = users.find(u => u.email === email && u.password === password);
+//   return user
+// }
+
+// app.post('/login', (req, res) => {
+//   let email =req.body.email
+//   let password =req.body.password
+//   let user = checkCredentials(email,password)
+//   console.log(user)
+//   if(!user){
+//     res.status(403).send("wrong credentials")
+//   }else{
+//     authToken =Math.random().toString()
+//     res.cookie("authToken",authToken)
+//     res.json({message:"authentification success"})
+//   }
+// })
+
+
+
+
+
+
+
+
+
+
--- requettes.js
+++ requettes.js
@@ -9,39 +9,56 @@ const client = new Client({
 client.connect()   
 
 
-
-     
-  
-
-function getAllProduct(client,callback){
+function GetAllProduct(client,callback){
     client.query("SELECT * FROM products",callback)
 }
-function getAll(client,callback){
+function GetAll(client,callback){
     client.query("SELECT * FROM products",callback )
 
 }
-function getProductId(client,[id],callback){
-    client.query("SELECT* from products Where id =$1",[id],callback )
+function GetProductId(client,[id],callback){
+    client.query("SELECT * FROM products WHERE id =$1",[id],callback )
 }
-function postProduct(product, client,callback){
+function PostProduct(product, client,callback){
     let valueproduct=[product.name,product.description,product.price,product.stock]
     client.query("INSERT INTO products (name, description, price, stock ) values ($1,$2,$3,$4)",valueproduct,callback )
 }
-function putProduct(id,product, client,callback){
+function PutProduct(id,product, client,callback){
     let valueproduct1=[product.name,product.description,product.price,product.stock,id]
     client.query("UPDATE products SET name=$1, description=$2, price=$3, stock=$4 WHERE id=$5",valueproduct1,callback )
 }
-function deleteProduct(id, client,callback){
+function DeleteProduct(id, client,callback){
     client.query("DELETE FROM products WHERE id=$1",[id],callback )
 }
+function addUser (client,newUser,callback){
+    let sql = `INSERT INTO users (firstname, lastname, email, password) VALUES ($1, $2, $3, $4)`;
+    client.query(sql,newUser,callback);
+}
+
+//les fonction sans hach code 2eme methode 
+// function PostRegister(users, client,callback){
+//     let valueuser=[users.firstname,users.lastname,users.email,users.password]
+//     client.query("INSERT INTO users (firstname, lastname, email, password ) values ($1,$2,$3,$4)",valueuser,callback )
+// }
+// function GetAllEmailUsers(client,callback){
+//     client.query("select email from users",callback)
+// }
+
+
+
 
 
 
 
 module.exports.client=client
-module.exports.getAllProduct = getAllProduct
-module.exports.getAll = getAll
-module.exports.getProductId = getProductId
-module.exports.postProduct = postProduct
-module.exports.putProduct = putProduct
-module.exports.deleteProduct = deleteProduct
+module.exports.GetAllProduct = GetAllProduct
+module.exports.GetAll = GetAll
+module.exports.GetProductId = GetProductId
+module.exports.PostProduct = PostProduct
+module.exports.PutProduct = PutProduct
+module.exports.DeleteProduct = DeleteProduct
+module.exports.addUser = addUser
+
+// module.exports.PostRegister = PostRegister
+// module.exports.GetAllEmailUsers = GetAllEmailUsers
+
